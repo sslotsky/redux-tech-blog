f96e34f2f938626763a278b1b004de9c357bc42b
Sam Slotsky <saxosamo@gmail.com>
2017-02-01 22:42:42 -0600

    hmr, not quite working

--- webpack.config.live.js
+++ webpack.config.live.js
@@ -4,6 +4,10 @@ var BrowserSyncPlugin = require('browser-sync-webpack-plugin')
 var _ = require('lodash')
 
 module.exports = _.extend(base, {
+  entry: [
+    'webpack-dev-server/client?http://0.0.0.0:8080', // WebpackDevServer host and port
+    './index.js'
+  ],
   devServer: {
     historyApiFallback: true
   },
@@ -12,6 +16,8 @@ module.exports = _.extend(base, {
       host: 'localhost',
       port: 3000,
       proxy: 'http://localhost:8080/'
+    }, {
+      reload: false
     }),
     new webpack.DefinePlugin({
       API_BASE: JSON.stringify('http://localhost:9999/api')

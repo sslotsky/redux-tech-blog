191a91441d1a5895a663261acb0fdc30cd78d685
Sam Slotsky <saxosamo@gmail.com>
2017-02-17 22:46:35 -0600

    place snippet editor in a fieldset

--- app/client/modules/posts/Snippet.jsx
+++ app/client/modules/posts/Snippet.jsx
@@ -37,17 +37,25 @@ export default class extends Component {
     const { language, text } = this.props.input.value
 
     return (
-      <div>
-        <select value={language} onChange={::this.changeLanguage}>
-          <option value='jsx'>JSX</option>
-          <option value='javascript'>JavaScript</option>
-          <option value='ruby'>Ruby</option>
-        </select>
-        <Snippet
-          language={language}
-          text={text}
-          onChange={::this.handleChange} />
-      </div>
+      <fieldset className="soft-half">
+        <h3>Snippet ({language})</h3>
+        <div className="form-group">
+          <label>Language</label>
+          <select value={language} onChange={::this.changeLanguage}>
+            <option value='jsx'>JSX</option>
+            <option value='javascript'>JavaScript</option>
+            <option value='ruby'>Ruby</option>
+          </select>
+        </div>
+        <div className="form-group">
+          <label>Text</label>
+          <Snippet
+            language={language}
+            text={text}
+            onChange={::this.handleChange}
+          />
+        </div>
+      </fieldset>
     )
   }
 }

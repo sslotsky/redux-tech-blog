4f8d61546edeff3422c5c76aa004f335c4d6dfc5
Sam Slotsky <saxosamo@gmail.com>
2017-02-19 22:17:41 -0600

    Preload posts

--- app/serverRendering.jsx
+++ app/serverRendering.jsx
@@ -10,6 +10,18 @@ import { compose, createStore, applyMiddleware } from 'redux'
 import routes from 'CLIENT/config/routes'
 import reducer from 'CLIENT/reducer'
 import template from './template'
+import Browse from 'MODULES/posts/components/Browse'
+import * as posts from 'SERVICES/posts'
+import { composables } from 'violet-paginator'
+
+const preload = (store, query, params) => ({
+  [Browse]: () => {
+    return posts.list().then(payload => {
+      const pageActions = composables({ listId: 'posts', preloaded: payload })
+      return store.dispatch(pageActions.initialize())
+    })
+  }
+})
 
 export default function(req, resp) {
   const memoryHistory = createMemoryHistory(req.path)
@@ -28,15 +40,21 @@ export default function(req, resp) {
     } else if (redirectLocation) {
       res.redirect(302, redirectLocation.pathname + redirectLocation.search)
     } else if (renderProps) {
+      const { components, query, params } = renderProps
+      const component = components[components.length - 1]
+      const handler = preload(store)[component]
+      const promise = handler ? handler(store, query, params) : Promise.resolve()
 
-      const html = renderToString(
-        <Provider store={store}>
-          <RouterContext {...renderProps}/>
-        </Provider>
-      )
+      promise.then(() => {
+        const html = renderToString(
+          <Provider store={store}>
+            <RouterContext {...renderProps}/>
+          </Provider>
+        )
 
-      const preloadedState = store.getState()
-      resp.send(template(html, preloadedState))
+        const preloadedState = store.getState()
+        resp.send(template(html, preloadedState))
+      })
     }
   })
 }
